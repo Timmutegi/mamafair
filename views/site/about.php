bf177a12caee045062ce7fe3ed7f1af70083cd24
CynthiaKamau <cynthia.kamau@students.jkuat.ac.ke>
2019-01-21 18:11:24 +0300

    Update about.php

--- views/site/about.php
+++ views/site/about.php
@@ -11,7 +11,21 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        <p>
+        Mama Fair is a solution to ensure that every child celebrates their fifth birthday 
+        According to statistics, 45 out of 1000 children die before the age of five maily due to communicable diseases that can be 
+        prevented when immunization schedules are followed strictly.
+        As Mama Fair, we have a number of solutions to ensure that parents can have useful information
+        during the pre and post natal period. This infromation ranges from personal hygiene, diet and general
+        questions that people ask. Credible information is essential in child upbringing.
+        <p> We also provide modern immunization charts that parents can use to track the immuniation schedules of 
+        their young ones as well as get notifications prior to the immunization day of children. Majortiy of children
+        miss immunization due to ignorance or their guardians forget their immunization dates.Children who do not et immunizations are
+        at the risk of getting diseases.</p>
+      
+        <p>MamaFair also connects parents to doctors who can advise on child related problems. This is because
+        a lot of time is spent in queus in hospitals due to a shortage of doctors . Instead of going to hospital,
+        MamaFair will prevent long queus in hospital by getting your concerns addressed at the comfort of your home.
     </p>
 
     <code><?= __FILE__ ?></code>
